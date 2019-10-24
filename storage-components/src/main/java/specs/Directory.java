e137e7736bacf2dd4b6e905758919790e0c3f358
Marko <Marko@DESKTOP-FFVEM1G>
2019-10-24 11:50:33 +0200

    user klasa

--- storage-components/src/main/java/specs/Directory.java
+++ storage-components/src/main/java/specs/Directory.java
@@ -1,5 +1,21 @@
 package specs;
 
 public interface Directory {
+	
+	public void createDirectory(String name, String path);
+	
+	public void deleteDirectory(String path);
+	
+	public void uploadDirectory(String path);
+	
+	public void downloadDirectory(String path);
+	
+	public void listAllFiles();
+	//Za listanje celog starage-a, mozda ne treba name
+	
+	public void listFiles(String name, String extension);
+	//Za listanje fajlova sa specificiranom ext ili ukoliko je prazno listaj sve fajlove sa zadatog direktorijuma.
+	
+	public void listDirectories(String name);
 
 }
--- storage-components/src/main/java/specs/File.java
+++ storage-components/src/main/java/specs/File.java
@@ -1,5 +1,13 @@
 package specs;
 
 public interface File {
+	
+	public void createFile(String name, String path, String fileExtension);
+
+	public void deleteFile(String path);
+	
+	public void uploadFile(String path, String FileExtension);
+	
+	public void downloadFile(String path);
 
 }
